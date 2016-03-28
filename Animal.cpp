15a8227e7c0b7a74a3775790c62e6426d8497368
bonganingobese <214513896@stu.ukzn.ac.za>
2016-03-28 08:14:18 +0200

    bird,bird classes and a drive program

--- Animal.cpp
+++ Animal.cpp
@@ -1,5 +1,7 @@
 #include <string>
+#include <iostream>
 using namespace std;
+
 class Animal
 {
 public:
@@ -12,11 +14,67 @@ public:
 	{
 
 	}
-	virtual string name() = 0;
-	virtual string does() = 0;
+	string name()
+	{
+		return Name;
+	}
+	string does()
+	{
+		return Does;
+	}
 private:
 	string Name;
 	string Does;
 
 };
+class Bird :public virtual Animal      
+{
+
+public:
+
+	Bird(string AniName, string job) :Animal(AniName, job)
+	{
+        
+	};
+	~Bird()
+	{
+
+	};
+	void print()
+	{
+cout <<name() << does() << endl;
+	}
+	
+};
+class Dog :public virtual Animal
+{
+
+public:
+
+	Dog(string AniName, string job) :Animal(AniName, job)
+	{
+		
+	};
+	~Dog()
+	{
+
+	};
+	void print()
+	{
+     cout << name() << does() << endl;
+	}
+
+};
+
+int main()
+{
+	Dog a("Dog","Bark");
+	Bird b("Bird","Fly");
+	
+	a.print();
+	b.print();
+
+
+
+}
 
